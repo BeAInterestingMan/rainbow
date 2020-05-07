0c5e479b4007aa74919ed82435bdedc1bb86166a
liuhu <1649471814@qq.com>
2020-05-07 14:35:53 +0800

    构建骨架

---
+++ rainbow-auth/src/main/java/com/rainbow/auth/AuthApplication.java
@@ -0,0 +1,15 @@
+package com.rainbow.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *  @Description 权限微服务启动类
+ *  @author liuhu
+ *  @Date 2020/5/7 14:23
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class);
+    }
+}
