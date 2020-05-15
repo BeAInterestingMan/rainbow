fa6076800cabf925f39c34c3b6cfb57efb588f0f
liuhu <1649471814@qq.com>
2020-05-15 18:17:50 +0800

    认证模块

---
+++ rainbow-common/rainbow-common-core/src/main/java/com/rainbow/common/core/entity/system/Role.java
@@ -0,0 +1,38 @@
+package com.rainbow.common.core.entity.system;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author MrBird
+ */
+@Data
+@TableName("t_role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = -1714476694755654924L;
+
+    @TableId(value = "ROLE_ID", type = IdType.AUTO)
+    private Long roleId;
+
+    @TableField(value = "ROLE_NAME")
+    private String roleName;
+
+    @TableField(value = "REMARK")
+    private String remark;
+
+    @TableField(value = "CREATE_TIME")
+    private Date createTime;
+
+    @TableField(value = "MODIFY_TIME")
+    private Date modifyTime;
+
+    private  String menuIds;
+
+}
